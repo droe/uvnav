7088ee399d8186e4f62ddde2cb9b0f91d520ee97
Daniel Roethlisberger <daniel@roe.ch>
2004-06-30 07:20:22 +0000

    svn2log.sh hinzugefuegt.

---
+++ tools/svn2log.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# UV Navigator - Auswertungsvisualisierung fuer Universum V
+# Copyright (C) 2004 Daniel Roethlisberger <roe@chronator.ch>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see http://www.gnu.org/copyleft/
+#
+# $Id$
+
+if [ ! -e ChangeLog ]; then
+	echo "Error: No ChangeLog to update." >&2
+fi
+
+echo "Updating ChangeLog..."
+svn -v log > ChangeLog
+echo "Done."
+
